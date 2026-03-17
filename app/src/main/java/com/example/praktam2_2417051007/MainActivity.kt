49fe1b3a74194a5d37ac724dcc3004f8af7fd8a4
Zahra-ayu <zahraayuazizah@gmail.com>
2026-03-17 10:13:59 +0700

    Tugas LKP 4

--- app/src/main/java/com/example/praktam2_2417051007/MainActivity.kt
+++ app/src/main/java/com/example/praktam2_2417051007/MainActivity.kt
@@ -3,23 +3,40 @@ package com.example.praktam2_2417051007
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation. Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.ConfirmationNumber
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.praktam2_2417051007.ui.theme.PrakTAM2_2417051007Theme
@@ -53,15 +70,28 @@ fun DaftarWisataScreen(){
 
 @Composable
 fun DetailScreen(wisata: Wisata){
+    var isFavorite by remember { mutableStateOf(false) }
     Column(modifier = Modifier.fillMaxSize()) {
-        Image(
-            painter = painterResource(id = wisata.imageRes),
-            contentDescription = wisata.nama,
-            modifier = Modifier
-                .fillMaxSize()
-                .height(200.dp),
-            contentScale = ContentScale.Crop
-        )
+        Box {
+            Image(
+                painter = painterResource(id = wisata.imageRes),
+                contentDescription = wisata.nama,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop
+            )
+            IconButton(
+                onClick = {isFavorite = !isFavorite},
+                modifier = Modifier.align(Alignment.TopEnd)
+            ){
+                Icon(
+                    imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+                    contentDescription = "Favorite",
+                    tint = if (isFavorite) Color.Red else Color.White
+                )
+            }
+        }
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(
@@ -73,14 +103,35 @@ fun DetailScreen(wisata: Wisata){
 
         Text(
             text = wisata.deskripsi,
-            style = MaterialTheme.typography.bodyLarge
+            style = MaterialTheme.typography.bodyLarge,
+            textAlign = TextAlign.Justify,
         )
         Spacer(modifier = Modifier.height(8.dp))
 
-        Text(
-            text = "Harga Tiket: Rp ${wisata.hargaTiketAwal} - ${wisata.hargaTiketAkhir}",
-            style = MaterialTheme.typography.bodyLarge
-        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = "Lokasi"
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = "Lokasi: ${wisata.lokasi}",
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Default.ConfirmationNumber,
+                contentDescription = "Tiket"
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = "Harga Tiket: Rp ${wisata.hargaTiketAwal} - ${wisata.hargaTiketAkhir}",
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
         Spacer(modifier = Modifier.height(16.dp))
 
         Button(
